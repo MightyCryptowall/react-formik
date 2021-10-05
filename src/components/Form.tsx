0528532e1eb51870d990e16a56ce947c153d1ea7
Ye Thiha <mightycryptowall@gmail.com>
2021-10-05 11:21:06 +0630

    form validation -a

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,39 +1,125 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ErrorMessage, Field, Formik, useFormik } from "formik";
 
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
-import { Field, Formik } from 'formik';
+interface FormProps {}
+const MySpecialField: React.FC<any> = ({ field }) => {
+  return <input {...field} className="border-2" />;
+};
 
-interface FormProps {
-    
-}
- 
+
+const validate = (values: any) => {
+  const errors:{firstName?:string, lastName?:string, email?:string} = {};
+  if(!values.firstName) {
+    errors.firstName = "Required"
+  }else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less"
+  }
+  if(!values.lastName) {
+    errors.lastName = "Required"
+  }else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less"
+  }
+  if(!values.email) {
+    errors.email = "Required"
+  }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address"
+  }
+
+  return errors;
+  
+};
+
+
+
 const Form: React.FC<FormProps> = () => {
 
-    const handleSubmit = (values: any) => {};
 
-    return ( 
-        <Formik initialValues={{}} onSubmit={handleSubmit} >
-        <Card style={{marginTop: "5em"}}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="140"
-            image="https://picsum.photos/300/200"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Formik
-            </Typography>
-          
-                <Field name="firstName"/>
-            
-          </CardContent>
-          <CardActions style={{ justifyContent: "flex-end" }}>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-        </Formik> 
-     );
-}
+  
+
+
+
+  
+  
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate: validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
  
+
+  return (
+    <Card style={{ marginTop: "5em" }}>
+      <form onSubmit={formik.handleSubmit}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image="https://picsum.photos/300/200"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Formik
+          </Typography>
+
+          <TextField
+            id="firstName"
+            name="firstName"
+            label="First Name"
+            variant="standard"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+            style={{paddingBottom: "1rem"}}
+            error = {(formik.errors.firstName) ? true : false}
+            helperText = {formik.errors.firstName}
+          />
+          <TextField
+            id="lastName"
+            name="lastName"
+            label="Last Name"
+            variant="standard"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+            style={{paddingBottom: "1rem"}}
+            error = {(formik.errors.firstName) ? true : false}
+            helperText = {formik.errors.firstName}
+          />
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            variant="standard"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            style={{paddingBottom: "1rem"}}
+            error = {(formik.errors.firstName) ? true : false}
+            helperText = {formik.errors.firstName}
+          />
+        </CardContent>
+        <CardActions style={{ justifyContent: "flex-end" }}>
+          <Button variant="contained" type="submit" size="small">Submit</Button>
+        </CardActions>
+      </form>
+    </Card>
+  );
+};
+
 export default Form;
