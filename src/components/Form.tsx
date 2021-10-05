f65fd847c77f87dfa8b7de0058bd96369e1a53ac
Ye Thiha <mightycryptowall@gmail.com>
2021-10-05 16:31:29 +0630

    form validation -u

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,41 +14,28 @@ const MySpecialField: React.FC<any> = ({ field }) => {
   return <input {...field} className="border-2" />;
 };
 
-
 const validate = (values: any) => {
-  const errors:{firstName?:string, lastName?:string, email?:string} = {};
-  if(!values.firstName) {
-    errors.firstName = "Required"
-  }else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less"
+  const errors: { firstName?: string; lastName?: string; email?: string } = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
   }
-  if(!values.lastName) {
-    errors.lastName = "Required"
-  }else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less"
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
   }
-  if(!values.email) {
-    errors.email = "Required"
-  }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address"
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
   }
 
   return errors;
-  
 };
 
-
-
 const Form: React.FC<FormProps> = () => {
-
-
-  
-
-
-
-  
-  
-
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -60,8 +47,6 @@ const Form: React.FC<FormProps> = () => {
       alert(JSON.stringify(values, null, 2));
     },
   });
-
- 
 
   return (
     <Card style={{ marginTop: "5em" }}>
@@ -85,9 +70,16 @@ const Form: React.FC<FormProps> = () => {
             type="text"
             onChange={formik.handleChange}
             value={formik.values.firstName}
-            style={{paddingBottom: "1rem"}}
-            error = {(formik.errors.firstName) ? true : false}
-            helperText = {formik.errors.firstName}
+            style={{ paddingBottom: "1rem" }}
+            error={
+              formik.errors.firstName && formik.touched.firstName ? true : false
+            }
+            helperText={
+              formik.errors.lastName && formik.touched.lastName
+                ? formik.errors.firstName
+                : ""
+            }
+            onBlur={formik.handleBlur}
           />
           <TextField
             id="lastName"
@@ -97,9 +89,16 @@ const Form: React.FC<FormProps> = () => {
             type="text"
             onChange={formik.handleChange}
             value={formik.values.lastName}
-            style={{paddingBottom: "1rem"}}
-            error = {(formik.errors.firstName) ? true : false}
-            helperText = {formik.errors.firstName}
+            style={{ paddingBottom: "1rem" }}
+            error={
+              formik.errors.lastName && formik.touched.lastName ? true : false
+            }
+            helperText={
+              formik.errors.lastName && formik.touched.lastName
+                ? formik.errors.lastName
+                : ""
+            }
+            onBlur={formik.handleBlur}
           />
           <TextField
             id="email"
@@ -109,13 +108,20 @@ const Form: React.FC<FormProps> = () => {
             type="email"
             onChange={formik.handleChange}
             value={formik.values.email}
-            style={{paddingBottom: "1rem"}}
-            error = {(formik.errors.firstName) ? true : false}
-            helperText = {formik.errors.firstName}
+            style={{ paddingBottom: "1rem" }}
+            error={formik.errors.email && formik.touched.email ? true : false}
+            helperText={
+              formik.errors.email && formik.touched.email
+                ? formik.errors.email
+                : ""
+            }
+            onBlur={formik.handleBlur}
           />
         </CardContent>
         <CardActions style={{ justifyContent: "flex-end" }}>
-          <Button variant="contained" type="submit" size="small">Submit</Button>
+          <Button variant="contained" type="submit" size="small">
+            Submit
+          </Button>
         </CardActions>
       </form>
     </Card>
